91a039936ccb71abdfa536e3bcfc259fb3c1adaf
michael.helena <michael.helena@cwi.com.br>
2022-09-24 15:19:27 -0300

    Inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
